5dab4d85e948938b8717d7f839f31fcf1b5e56d9
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2021-03-08 18:33:31 -0500

    Added edge cases

--- Labs/Lab04/binaryMantissa.cpp
+++ Labs/Lab04/binaryMantissa.cpp
@@ -7,8 +7,11 @@ string ToBinaryMantissa(double radix) {
     /*
         Convert the portion of a number after it's decimal point to binary.
     */
+    if (radix >= 1) {
+        return "";
+    }
     string binaryMantissa = "0.";
-    while (radix > 0) {
+    while (radix > 0 and binaryMantissa.length() < 12) {
         radix *= 2;
         if (radix >= 1) {
             binaryMantissa += "1";
@@ -21,6 +24,6 @@ string ToBinaryMantissa(double radix) {
 }
 
 int main() {
-    cout << ToBinaryMantissa(0.625) << endl;
+    cout << ToBinaryMantissa(0.8) << endl;
     return 0;
 }
